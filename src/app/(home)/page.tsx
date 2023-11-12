936791caf99d2951d1e483f67bc21f7e038b43c5
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-11-12 16:32:33 -0300

    initial config in product page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { foods } from "@/data/foods";
 import { CardItem } from "./components/CardItem";
 import BannerSlider from "./components/BannerSlider";
-import { useState } from "react";
 import { useFood } from "@/context/FoodContext";
 
 export default function Home() {
-  const food = useFood();
+  const foodCtx = useFood();
 
   return (
     <div className="container mx-auto flex flex-col gap-4 p-2">
       <BannerSlider />
       <div className="flex flex-col gap-2">
-        {food?.data.map((item) => <CardItem key={item.id} item={item} />)}
+        {foodCtx?.data.map((item) => <CardItem key={item.id} item={item} />)}
       </div>
     </div>
   );
