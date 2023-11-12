c45e1e4a62e25aa834e7a1b460dcf8b2b47f0a94
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-11-12 14:12:42 -0300

    update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/ui/Header";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["700", "600"] });
 
 export const metadata: Metadata = {
-    title: "Castelo Encantado",
-    description: "Cardápio Virtual",
+  title: "Castelo Encantado",
+  description: "Cardápio Virtual",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="pt-BR">
-            <body className={montserrat.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="pt-BR">
+      <body className={montserrat.className}>
+        <div className="flex h-full flex-col">
+          <Header />
+          <div className="flex-1">{children}</div>
+          <footer>Um footer...</footer>
+        </div>
+      </body>
+    </html>
+  );
 }
