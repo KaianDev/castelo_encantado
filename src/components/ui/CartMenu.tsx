31d27e9c7e58aa14332ebcd1af515267f2771153
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-11-12 14:13:20 -0300

    update in cart menu

--- src/components/ui/CartMenu.tsx
+++ src/components/ui/CartMenu.tsx
@@ -5,7 +5,12 @@ const CartMenu = () => {
   return (
     <Sheet>
       <SheetTrigger>
-        <ShoppingCart size={30} />
+        <div className="relative">
+          <ShoppingCart size={40} />
+          <div className=" absolute -bottom-2 -right-2 flex h-6 w-6 items-center justify-center rounded-full bg-red-castle text-sm text-white">
+            20
+          </div>
+        </div>
       </SheetTrigger>
       <SheetContent className="w-4/5 sm:w-[540px]">
         <SheetHeader>Carrinho</SheetHeader>
